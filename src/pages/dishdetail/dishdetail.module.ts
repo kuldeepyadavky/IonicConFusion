14bad706b93fd63ad76c253927b1320412dd39c4
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2020-09-24 11:02:32 +0530

    Ionic Navigation

---
+++ src/pages/dishdetail/dishdetail.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { DishdetailPage } from "./dishdetail";
+
+@NgModule({
+  declarations: [DishdetailPage],
+  imports: [IonicPageModule.forChild(DishdetailPage)],
+})
+export class DishdetailPageModule {}
