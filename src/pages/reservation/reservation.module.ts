c5e085b4774de5082bd16188e622de5778e110ba
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2020-09-25 17:02:17 +0530

    Grid

--- src/pages/reservation/reservation.module.ts
+++ src/pages/reservation/reservation.module.ts
@@ -1,13 +1,9 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ReservationPage } from './reservation';
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { ReservationPage } from "./reservation";
 
 @NgModule({
-  declarations: [
-    ReservationPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ReservationPage),
-  ],
+  declarations: [ReservationPage],
+  imports: [IonicPageModule.forChild(ReservationPage)],
 })
 export class ReservationPageModule {}
